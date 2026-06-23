64b6bbbf895bbc58d5f69a049e6edebeacf655d5
Yasmin <miiaraujo11@gmail.com>
2026-06-23 20:47:54 -0300

    Adiciona frontend

---
+++ front/src/componentes/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+
+
+import "@/componentes/Footer/Footer.css";
+export default function Footer(){
+    return(
+        <footer className='footer'>
+            <section>
+                <p style={{textAlign: "center"}}>Feito por <a href="https://www.linkedin.com/in/sofia-nogueira-batista-740870381/" target='_blank'>Sofia Nogueira</a> e <a href="https://www.linkedin.com/in/yasminara%C3%BAjo/" target='_blank'>Yasmin Araújo</a></p>
+            </section>
+        </footer>
+    );
+}
